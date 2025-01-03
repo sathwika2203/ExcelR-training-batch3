2d683cc88e152f3bccef107aea88980cb4447df9
sathwika2203 <devarasathwika@gmail.com>
2025-01-03 10:43:32 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,11 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        String username = "Sathwika";
+        String password = "2005";
+        while((username =="Sathwika") && (password == "2005")) {
+            System.out.println("Hello user! Happy New Year");
+        
+        break;
+        }
+    }
+}
