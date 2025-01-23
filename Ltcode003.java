92ba6ce847b7fe1ecc811eee700c9ce160e8b4f8
sathwika2203 <devarasathwika@gmail.com>
2025-01-23 12:29:15 +0530

    string to integers using leetcode

--- Ltcode003.java
+++ Ltcode003.java
@@ -1,20 +1,21 @@
-class Solution {
+package Core_Java;
+
+import java.util.Scanner;
+
+class solution {
     public int myAtoi(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
 
         int i = 0, n = s.length();
-
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
 
-
         if (i == n) {
             return 0;
         }
-
 
         int sign = 1;
         if (s.charAt(i) == '-' || s.charAt(i) == '+') {
@@ -22,11 +23,9 @@ class Solution {
             i++;
         }
 
-
         int result = 0;
         while (i < n && Character.isDigit(s.charAt(i))) {
             int digit = s.charAt(i) - '0';
-
 
             if (result > (Integer.MAX_VALUE - digit) / 10) {
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
@@ -37,5 +36,15 @@ class Solution {
         }
 
         return result * sign;
+    }
+}
+public class LTCode003{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String str = sc.next();
+        solution sol = new solution();
+        int result = sol.myAtoi(str);
+        System.out.println(result);
     }
 }
