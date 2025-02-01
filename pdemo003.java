2cf679c9eb2a2f7689b6f516c6ce4aaf0fc243a8
sathwika2203 <devarasathwika@gmail.com>
2025-02-01 12:48:54 +0530

    if,elseif,else

---
+++ pdemo003.java
@@ -0,0 +1,16 @@
+public class pdemo003{
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 67;
+        int num3 = 34;
+        if(num1 > num2){
+            System.out.println("num1 is greater");
+        }
+        else if(num2 > num3){
+            System.out.println("num2 is greater");
+        }
+        else{
+            System.out.println("num3 is greater");
+        }
+    }
+}
