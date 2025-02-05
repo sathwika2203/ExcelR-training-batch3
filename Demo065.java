61e0ff2afe5294a3a8134a69eb150acf63357d1b
sathwika2203 <devarasathwika@gmail.com>
2025-02-05 12:36:42 +0530

    to print array elements

---
+++ Demo065.java
@@ -0,0 +1,8 @@
+class Demo065{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
