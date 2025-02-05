af06289265ad673492ae75fcb4451a79afedb4cd
sathwika2203 <devarasathwika@gmail.com>
2025-02-05 12:10:52 +0530

    to print 1st,2nd and last element of an array

---
+++ Demo062.java
@@ -0,0 +1,9 @@
+class Demo062{
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int n = arr.length;
+        System.out.println("The first element of array is "+arr[0]);
+        System.out.println("The second element of array is "+arr[1]);
+        System.out.println("The last element of array is "+arr[n-1]);
+    }
+}
