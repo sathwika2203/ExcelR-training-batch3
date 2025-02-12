4aeef33290fef50e5fc44083edbdb6bc0344fa23
sathwika2203 <devarasathwika@gmail.com>
2025-02-12 21:36:28 +0530

    While loop

---
+++ Pdemo005.java
@@ -0,0 +1,10 @@
+public class Pdemo005 {
+    public static void main(String[] args) {
+        String Username = "Sathwika";
+        String pwd = "sath123";
+        while((Username == "Sathwika") && (pwd == "sath123")){
+            System.out.println("Hi Sathwika");
+        
+        break;
+    }}
+}
